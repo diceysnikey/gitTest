8683ba2eaf6c6c50dcadea6e3d5564faec51219d
Erdin Zeller <erdinzeller@gmail.com>
2024-12-18 22:35:47 +0100

    added feature

--- username.py
+++ username.py
@@ -2,5 +2,7 @@ while True:
     username = input("Enter your username (Under 12 characters): ")
     if len(username) >= 12:
         print("Your username is too long!")
+    elif len(username) == 0:
+        print("Enter a username!")
     else:
         break
