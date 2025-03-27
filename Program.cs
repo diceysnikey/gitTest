f0663c5fd1c067594f9ca4475e74530442d1275e
Erdin <99503244+diceysnikey@users.noreply.github.com>
2025-03-27 14:58:35 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Aufgaben;
+
+public class Program
+{
+    static void Main()
+    {
+        bool repeat = true;
+        while (repeat)
+        {
+            Console.WriteLine("What do you want to do?" +
+                              "\n1. Show Tasks" +
+                              "\n2. Add Task" +
+                              "\n3. Complete Task" +
+                              "\n4. Delete Task\n");
+            Console.Write("Enter a number: ");
+
+            string answer = Console.ReadLine().ToLower();
+            switch (answer)
+            {
+                case "1":
+                case "show":
+                    TaskItem.ShowTasks();
+                    break;
+                case "2":
+                case "add":
+                    TaskItem.AddTask();
+                    break;
+                case "3":
+                case "complete":
+                    TaskItem.CompleteTask();
+                    break;
+                case "4":
+                case "delete":
+                    TaskItem.DeleteTask();
+                    break;
+                default:
+                    Console.WriteLine("Input not valid. Please enter a number!");
+                    break;
+            }
+            Console.Write("Do you want to continue? (y/n): ");
+            if (Console.ReadLine()?.ToLower() == "n")
+            {
+                repeat = false;
+            }
+        }
+    }
+}
---
+++ Taskitem.cs
@@ -0,0 +1,138 @@
+﻿namespace Aufgaben;
+
+public class TaskItem
+{
+    private string Title { get; }
+    private bool IsCompleted { get; set; }
+
+    private static List<TaskItem> _taskCollection = [];
+    private const string FilePath = @"C:\Users\Erdin Zeller\RiderProjects\cstest\Aufgaben\AllTasks.txt";
+
+    private TaskItem(string title, bool isCompleted)
+    {
+        Title = title;
+        IsCompleted = isCompleted;
+    }
+
+    private static string ReturnCapitalizedName(string name)
+    {
+        name = char.ToUpper(name[0]) + name.Substring(1);
+        return name;
+    }
+
+    private string ReturnTextToString(string name, bool isCompleted)
+    {
+        return $"{name}: {isCompleted}";
+    }
+
+    private static void OverwriteAllText(List<TaskItem> tasks)
+    {
+        File.WriteAllText(FilePath, "");
+        for (int i = 0; i < tasks.Count; i++)
+        {
+            File.AppendAllText(FilePath,
+                $"{tasks[i].Title}: {tasks[i].IsCompleted}" +
+                (i < tasks.Count - 1 ? Environment.NewLine : ""));
+        }
+    }
+
+    private static List<TaskItem> ReturnLinesFromFile()
+    {
+        _taskCollection.Clear();
+        var lines = File.ReadAllLines(FilePath)
+            .Where(x => !string.IsNullOrWhiteSpace(x));
+        
+        foreach (var line in lines)
+        {
+            var parts = line.Split(": ");
+            
+            var fileTitle = parts[0];
+            var fileIsCompleted = bool.Parse(parts[1]);
+            _taskCollection.Add(new TaskItem(fileTitle, fileIsCompleted));
+        }
+
+        return _taskCollection;
+    }
+    public static void ShowTasks()
+    {
+        _taskCollection = ReturnLinesFromFile();
+        foreach (var task in _taskCollection)
+        {
+            Console.WriteLine($"{task.Title}: {task.IsCompleted}");
+        }
+    }
+    public static void AddTask()
+    {
+        Console.Write("What is your new task called?: ");
+        var newTaskName = Console.ReadLine();
+        if (newTaskName != null)
+        {
+            var newTask = new TaskItem(newTaskName, false);
+            if (new FileInfo(FilePath).Length == 0)
+            {
+                File.AppendAllText(FilePath, newTask.ReturnTextToString
+                                                (ReturnCapitalizedName(newTaskName), false));
+            }
+            else
+            {
+                File.AppendAllText(FilePath, Environment.NewLine
+                                             + newTask.ReturnTextToString
+                                                 (ReturnCapitalizedName(newTaskName), false));
+            }
+        }
+    }
+
+    public static void CompleteTask()
+    {
+        Console.Write("What Task did you complete?: ");
+        var completedTaskName = Console.ReadLine();
+
+        while (completedTaskName == null)
+        {
+            Console.WriteLine("You need to enter a Task name!");
+            completedTaskName = Console.ReadLine();
+        }
+        
+        completedTaskName = ReturnCapitalizedName(completedTaskName);
+        _taskCollection = ReturnLinesFromFile();
+        
+        TaskItem? foundTask = _taskCollection.FirstOrDefault(t => t.Title == completedTaskName);
+        if (foundTask != null)
+        {
+            foundTask.IsCompleted = true;
+            OverwriteAllText(_taskCollection);
+        }
+        else
+        {
+            Console.WriteLine("Task could not be found!");
+        }
+    }
+
+    public static void DeleteTask()
+    {
+        Console.Write("What Task do you want to remove?: ");
+        var deleteTaskName = Console.ReadLine();
+        while (deleteTaskName == null)
+        {
+            Console.WriteLine("You need to enter a Task name!");
+            deleteTaskName = Console.ReadLine();
+        }
+        
+        deleteTaskName = ReturnCapitalizedName(deleteTaskName);
+        _taskCollection = ReturnLinesFromFile();
+        
+        TaskItem? foundTask = _taskCollection.FirstOrDefault(t => t.Title == deleteTaskName);
+        if (foundTask != null)
+        {
+            _taskCollection.Remove(foundTask);
+            OverwriteAllText(_taskCollection);
+        }
+        else
+        {
+            Console.WriteLine("Task could not be found!");
+        }
+        
+    }
+}
+
+
